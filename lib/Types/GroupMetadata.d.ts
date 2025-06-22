8f4dd378bc56c2f4fb2d59edbb85a292d9d8cb65
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-06-21 22:55:17 -0600

    Update GroupMetadata.d.ts

--- lib/Types/GroupMetadata.d.ts
+++ lib/Types/GroupMetadata.d.ts
@@ -1,10 +1,9 @@
 import { Contact } from './Contact'
 
 export type GroupParticipant = Contact & {
-    isAdmin?: boolean
-    isSuperAdmin?: boolean
-    admin?: 'admin' | 'superadmin' | 'member' | null
-    lid?: string // Added for lid support
+	isAdmin?: boolean
+	isSuperAdmin?: boolean
+	admin?: 'admin' | 'superadmin' | null
 }
 
 export type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote' | 'modify'
@@ -14,66 +13,69 @@ export type RequestJoinAction = 'created' | 'revoked' | 'rejected'
 export type RequestJoinMethod = 'invite_link' | 'linked_group_join' | 'non_admin_add' | undefined
 
 export interface GroupMetadata {
-    id: string
-    /** group uses 'lid' or 'pn' to send messages */
-    addressingMode: 'pn' | 'lid' | string // Made more flexible
-    owner: string | undefined
-    ownerJid?: string // For lid mode
-    ownerCountry?: string // Added from second version
-    subject: string
-    /** group subject owner */
-    subjectOwner?: string
-    subjectOwnerJid?: string // For lid mode
-    /** group subject modification date */
-    subjectTime?: number
-    creation?: number
-    desc?: string
-    descOwner?: string
-    descOwnerJid?: string // For lid mode
-    descId?: string
-    descTime?: number
-    /** if this group is part of a community, it returns the jid of the community to which it belongs */
-    linkedParent?: string
-    /** is set when the group only allows admins to change group settings */
-    restrict?: boolean
-    /** is set when the group only allows admins to write messages */
-    announce?: boolean
-    /** is set when the group also allows members to add participants */
-    memberAddMode?: boolean
-    /** Request approval to join the group */
-    joinApprovalMode?: boolean
-    /** is this a community */
-    isCommunity?: boolean
-    /** is this the announce of a community */
-    isCommunityAnnounce?: boolean
-    /** number of group participants */
-    size?: number
-    participants: GroupParticipant[]
-    picture?: string // Added from second version
-    ephemeralDuration?: number
-    inviteCode?: string
-    /** the person who added you to group or changed some setting in group */
-    author?: string
+	id: string
+	/** group uses 'lid' or 'pn' to send messages */
+	addressingMode: 'pn' | 'lid'
+	owner: string | undefined
+	ownerJid?: string | undefined
+	subject: string
+	/** group subject owner */
+	subjectOwner?: string
+	subjectOwnerJid?: string
+	/** group subject modification date */
+	subjectTime?: number
+	creation?: number
+	desc?: string
+	descOwner?: string
+	descOwnerJid?: string
+	descId?: string
+	descTime?: number
+	/** if this group is part of a community, it returns the jid of the community to which it belongs */
+	linkedParent?: string
+	/** is set when the group only allows admins to change group settings */
+	restrict?: boolean
+	/** is set when the group only allows admins to write messages */
+	announce?: boolean
+	/** is set when the group also allows members to add participants */
+	memberAddMode?: boolean
+	/** Request approval to join the group */
+	joinApprovalMode?: boolean
+	/** is this a community */
+	isCommunity?: boolean
+	/** is this the announce of a community */
+	isCommunityAnnounce?: boolean
+	/** number of group participants */
+	size?: number
+	// Baileys modified array
+	participants: GroupParticipant[]
+	ephemeralDuration?: number
+	inviteCode?: string
+	/** the person who added you to group or changed some setting in group */
+	author?: string
+
+	// NUEVOS CAMPOS AGREGADOS DESDE VERSIONES MODERNAS DE BAILEYS
+	/** grupo en modo solamente lectura */
+	support?: {
+		admin?: boolean
+		memberAddMode?: boolean
+	}
+	/** JID del grupo padre si forma parte de una comunidad */
+	parentCommunityJid?: string
+	/** si la aprobación de ingreso está habilitada */
+	joinRequestApprovalMode?: boolean
+	/** nombre visible del grupo (distinto al subject si lo hay) */
+	displaySubject?: string
+	/** tipo de comunidad: announce, regular, etc. */
+	communityType?: 'announcement' | 'regular'
 }
 
 export interface WAGroupCreateResponse {
-    status: number
-    gid?: string
-    participants?: [{ [key: string]: {} }]
+	status: number
+	gid?: string
+	participants?: [{ [key: string]: {} }]
 }
 
 export interface GroupModificationResponse {
-    status: number
-    participants?: { [key: string]: {} }
-}
-
-// Additional types for better lid support
-export interface GroupMetadataLid extends GroupMetadata {
-    addressingMode: 'lid'
-    ownerJid: string
-    participants: (GroupParticipant & { lid: string })[]
-}
-
-export interface GroupMetadataPn extends GroupMetadata {
-    addressingMode: 'pn'
+	status: number
+	participants?: { [key: string]: {} }
 }
