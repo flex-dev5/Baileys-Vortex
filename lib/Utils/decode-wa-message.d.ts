28fb4f02b2fad46d1bae7d2776e269b2b0974f78
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-06-21 23:10:56 -0600

    Update decode-wa-message.d.ts

--- lib/Utils/decode-wa-message.d.ts
+++ lib/Utils/decode-wa-message.d.ts
@@ -28,13 +28,77 @@ export declare const NACK_REASONS: {
  * @note this will only parse the message, not decrypt it
  */
 export declare function decodeMessageNode(stanza: BinaryNode, meId: string, meLid: string): {
-    fullMessage: proto.IWebMessageInfo
+    fullMessage: proto.IWebMessageInfo & {
+        key: {
+            remoteJid: string
+            fromMe: boolean
+            id: string
+            participant?: string
+            senderLid?: string
+            senderPn?: string
+            participantLid?: string
+            newsletter_server_id?: number
+        }
+        messageTimestamp: number
+        pushName?: string
+        broadcast: boolean
+        newsletter: boolean
+        newsletter_server_id?: number
+        status?: proto.WebMessageInfo.Status
+        platform?: string
+        verifiedBizName?: string
+        multicast?: boolean
+        metaInfo?: {
+            targetID: string
+            targetSender?: string
+        }
+        botInfo?: {
+            editType: string
+            editTargetID: string
+            editSenderTimestampMS: string
+        }
+        message?: proto.IMessage
+        messageStubType?: proto.WebMessageInfo.StubType
+        messageStubParameters?: string[]
+    }
     author: string
     sender: string
 }
 
 export declare const decryptMessageNode: (stanza: BinaryNode, meId: string, meLid: string, repository: SignalRepository, logger: ILogger) => {
-    fullMessage: proto.IWebMessageInfo
+    fullMessage: proto.IWebMessageInfo & {
+        key: {
+            remoteJid: string
+            fromMe: boolean
+            id: string
+            participant?: string
+            senderLid?: string
+            senderPn?: string
+            participantLid?: string
+            newsletter_server_id?: number
+        }
+        messageTimestamp: number
+        pushName?: string
+        broadcast: boolean
+        newsletter: boolean
+        newsletter_server_id?: number
+        status?: proto.WebMessageInfo.Status
+        platform?: string
+        verifiedBizName?: string
+        multicast?: boolean
+        metaInfo?: {
+            targetID: string
+            targetSender?: string
+        }
+        botInfo?: {
+            editType: string
+            editTargetID: string
+            editSenderTimestampMS: string
+        }
+        message?: proto.IMessage
+        messageStubType?: proto.WebMessageInfo.StubType
+        messageStubParameters?: string[]
+    }
     category: string
     author: string
     decrypt(): Promise<void>
